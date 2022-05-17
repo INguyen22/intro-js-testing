0ce2077c3dbb38f93bd239186e9fae72a963729e
Ivy Nguyen <animeweirdo24@gmail.com>
2022-05-17 14:28:06 -0600

    do tweet test

--- tweet.js
+++ tweet.js
@@ -1,5 +1,23 @@
 class Tweet {
-
+  constructor(userName, context, link,) {
+    this.sender = userName;
+    this.content = context;
+    this.link = link || 'no link provided';
+    this.likes = 0;
+    this.retweets = 0;
+    this.replies = [];
+    this.replyCount = 0;
+}
+like(numLikes) {
+  this.likes = this.likes + 1
+}
+retweet() {
+  this.retweets = this.retweets + 1
+}
+reply(reply) {
+  this.replies.push(reply)
+  this.replyCount = this.replies.length
+}
 }
 
 module.exports = Tweet;
